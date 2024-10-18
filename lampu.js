c0af8847bca509745419ffe3542bce66619d5858
Aam <140715868+Aammahrojan@users.noreply.github.com>
2024-10-18 09:25:37 +0700

    Add files via upload

---
+++ lampu.js
@@ -0,0 +1,12 @@
+const body = document.body;
+  const gambar = document.getElementById("gambar");
+  gambar.style.backgroundColor = 'black';
+  
+
+function on(){
+  gambar.src = 'gambar/on.jpg';
+}
+
+function off(){
+  gambar.src = 'gambar/off.jpg';
+}
